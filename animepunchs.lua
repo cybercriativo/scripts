6a8ec3e07398844c4fe84383c8dea9e24b0dae3c
cybercriativo <90424578+cybercriativo@users.noreply.github.com>
2024-01-18 22:29:15 -0300

    Update animepunchs.lua

--- animepunchs.lua
+++ animepunchs.lua
@@ -1,143 +1,252 @@
--- Lista de membros autorizados
-local membros_autorizados = {"EspacialCar", "NoobdoRodox"}
+getgenv().getCardRaid = false;
+getgenv().getRaidBoss = false;
 
--- Obtém o nome do jogador no jogo
-local jogador = game.Players.LocalPlayer
-local nome_do_jogador = jogador.Name
+function doCardRaid()
+
+    spawn(function() 
+        
+        while getCardRaid == true do
+            --Mundo 1
+
+            local args = {
+                [1] = "Teleport",
+                [2] = "Spawn",
+                [3] = "Leaf City"
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(1)
+
+            local args = {
+                [1] = "Attack",
+                [2] = "Click",
+                [3] = {
+                    ["Enemy"] = workspace.Server.Enemies.World:FindFirstChild("Leaf City").Orochimare,
+                    ["Type"] = "World"
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(3)
+            
+            --Mundo 2
+            local args = {
+                [1] = "Teleport",
+                [2] = "Spawn",
+                [3] = "Desert Piece"
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(1)
 
 
+            local args = {
+                [1] = "Attack",
+                [2] = "Click",
+                [3] = {
+                    ["Enemy"] = workspace.Server.Enemies.World:FindFirstChild("Desert Piece"):FindFirstChild("White Beard"),
+                    ["Type"] = "World"
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(3)
 
--- Função para verificar se o jogador está na lista de membros autorizados
-local function jogadorEstaAutorizado()
-    for _, membro in ipairs(membros_autorizados) do
-        print("Comparando", nome_do_jogador, membro)
-        if nome_do_jogador == membro then
-            return true
-        end
+            --Mundo 3
+            local args = {
+                [1] = "Teleport",
+                [2] = "Spawn",
+                [3] = "Slayer Village"
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(1)            
+
+
+            local args = {
+                [1] = "Attack",
+                [2] = "Click",
+                [3] = {
+                    ["Enemy"] = workspace.Server.Enemies.World:FindFirstChild("Slayer Village").Yoreche,
+                    ["Type"] = "World"
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(3)
+
+
+            local args = {
+                [1] = "Teleport",
+                [2] = "Spawn",
+                [3] = "Cursed Academy"
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(1)            
+
+
+            local args = {
+                [1] = "Attack",
+                [2] = "Click",
+                [3] = {
+                    ["Enemy"] = workspace.Server.Enemies.World:FindFirstChild("Cursed Academy").Sukene,
+                    ["Type"] = "World"
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(3)
+
+            local args = {
+                [1] = "Teleport",
+                [2] = "Spawn",
+                [3] = "Dragon City"
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(1)            
+            
+
+            local args = {
+                [1] = "Attack",
+                [2] = "Click",
+                [3] = {
+                    ["Enemy"] = workspace.Server.Enemies.World:FindFirstChild("Dragon City").Broli,
+                    ["Type"] = "World"
+                }
+            }
+            
+            game:GetService("ReplicatedStorage").Bridge:FireServer(unpack(args))
+            wait(10)
+
+        end        
+        
+
+    end)
+
+end
+
+function teleportTo(placeCFrame)
+    local plyr = game.Players.LocalPlayer;
+    if plyr.Character then
+        plyr.Character.HumanoidRootPart.CFrame = placeCFrame;
     end
 end
 
--- Lógica principal
-if jogadorEstaAutorizado() then
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+function doRaidBoss()
+    
+    spawn(function()
+        
+        local bossPath = workspace.Client.Enemies.RaidBoss;
 
-    OrionLib:MakeNotification({
-        Name = "Anime Punch Simulator",
-        Content = "Anime Punch Sim",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
+        while getRaidBoss == true do
 
+            -- Itera pelos filhos do caminho dos bosses
+            for _, boss in pairs(bossPath:GetChildren()) do
+                -- Verifica se o filho é um Model e se possui uma RootPart
+                if boss:IsA("Model") and boss:FindFirstChild("HumanoidRootPart") then
+                    local rootPart = boss.HumanoidRootPart
 
-    local Window = OrionLib:MakeWindow({Name = "Campeao Sim", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "Orion"})
+                    -- Obtém o CFrame da RootPart
+                    local rootPartCFrame = rootPart.CFrame
 
-    --Player Tab--
+                    -- Imprime a matriz de transformação do CFrame
+                    print("CFrame da RootPart do boss:", rootPartCFrame)
+                    
+                    function teleportTo(placeCFrame)
+                        local plyr = game.Players.LocalPlayer;
+                        if plyr.Character then
+                            plyr.Character.HumanoidRootPart.CFrame = placeCFrame;
+                        end
+                    end
+                    teleportTo(rootPartCFrame)
+                end
 
-    local PlayerTab = Window:MakeTab({
-        Name = "Player",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-
-    local PlayerSection = PlayerTab:AddSection({
-        Name = "Auto Click"
-    })
-
-
-    PlayerSection:AddToggle({
-        Name = "Auto Click",
-        Default = false,
-        Callback = function(Value)
-            autoClick = Value;
-            if autoClick == true then
-                getClick();
             end
-        end    
-    })
 
-    --Player Tab End--
+        end
 
-    --Settings Tab--
+    end)
 
-    local SettingsTab = Window:MakeTab({
-        Name = "Settings",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-
-    local SettingsSection = SettingsTab:AddSection({
-        Name = "Settings"
-    })
-
-    SettingsSection:AddButton({
-        Name = "Destroy UI",
-        Callback = function()
-            OrionLib:Destroy()
-        end    
-    })
-
-    --Settings End--
-
-    OrionLib:Init()
-else
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
-    OrionLib:MakeNotification({
-        Name = "Anime Punch Simulator",
-        Content = "Anime Punch Sim",
-        Image = "rbxassetid://4483345998",
-        Time = 5
-    })
-
-
-    local Window = OrionLib:MakeWindow({Name = "Campeao Sim Free", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "Orion"})
-
-    --Player Tab--
-
-    local PlayerTab = Window:MakeTab({
-        Name = "Player",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-
-    local PlayerSection = PlayerTab:AddSection({
-        Name = "Farm Card"
-    })
-
-
-    PlayerSection:AddToggle({
-        Name = "Farm Card Raid",
-        Default = false,
-        Callback = function(Value)
-            getCardRaid = Value;
-            if getCardRaid == true then
-                doCardRaid();
-            end
-        end    
-    })
-
-    --Player Tab End--
-
-    --Settings Tab--
-
-    local SettingsTab = Window:MakeTab({
-        Name = "Settings",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-
-    local SettingsSection = SettingsTab:AddSection({
-        Name = "Settings"
-    })
-
-    SettingsSection:AddButton({
-        Name = "Destroy UI",
-        Callback = function()
-            OrionLib:Destroy()
-        end    
-    })
-
-    --Settings End--
-
-    OrionLib:Init()
 end
+
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+OrionLib:MakeNotification({
+    Name = "Anime Punch Simulator",
+    Content = "Anime Punch Sim",
+    Image = "rbxassetid://4483345998",
+    Time = 5
+})
+
+
+local Window = OrionLib:MakeWindow({Name = "Campeao Sim", HidePremium = false, IntroEnabled = false, SaveConfig = true, ConfigFolder = "Orion"})
+
+--Player Tab--
+
+local FarmTab = Window:MakeTab({
+    Name = "Player",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local PlayerSection = PlayerTab:AddSection({
+    Name = "Farm Card"
+})
+
+
+FarmTab:AddToggle({
+    Name = "Farm Card Raid",
+    Default = false,
+    Callback = function(Value)
+        getCardRaid = Value;
+        if getCardRaid == true then
+            doCardRaid();
+        end
+    end    
+})
+
+FarmTab:AddToggle({
+    Name = "Auto Raid Boss",
+    Default = false,
+    Callback = function(Value)
+        getCardRaid = Value;
+        if getRaidBoss == true then
+            doRaidBoss();
+        end
+    end 
+})
+
+FarmTab:AddButton({
+    Name = "Destroy UI",
+    Callback = function()
+        OrionLib:Destroy()
+    end    
+})
+
+--Farm Tab End--
+
+--Settings Tab--
+
+/*local SettingsTab = Window:MakeTab({
+    Name = "Settings",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+local SettingsSection = SettingsTab:AddSection({
+    Name = "Settings"
+})
+
+SettingsSection:AddButton({
+    Name = "Destroy UI",
+    Callback = function()
+        OrionLib:Destroy()
+    end    
+})*/
+
+--Settings End--
+
+OrionLib:Init()
